f901e36a0aa5ec871bd275a70e6e0b4c440ff69b
readeral <areader0@gmail.com>
2020-02-07 12:52:04 +1100

    Add option to disable create button
    At the moment the only way to disable create button is to add a second type (be it fake or otherwise) to the types array. This pull request would give an option of 'createButton' which would default to 'true' but could be set to 'false' if you don't want the intent.
    
    A more explicit way of stopping a button being produced.

--- src/DocumentList.js
+++ src/DocumentList.js
@@ -29,6 +29,7 @@ class DocumentList extends React.Component {
     queryParams: PropTypes.object, // eslint-disable-line react/forbid-prop-types
     order: PropTypes.string,
     limit: PropTypes.number,
+    createButton: PropTypes.bool,
     createButtonText: PropTypes.string
   }
 
@@ -39,6 +40,7 @@ class DocumentList extends React.Component {
     types: null,
     query: null,
     queryParams: {},
+    createButton: true,
     createButtonText: null
   }
 
@@ -88,7 +90,7 @@ class DocumentList extends React.Component {
 
 
   render() {
-    const {title, types, createButtonText} = this.props
+    const {title, types, createButton, createButtonText} = this.props
     const {documents, loading, error} = this.state
 
     return (
@@ -121,7 +123,7 @@ class DocumentList extends React.Component {
             })}
           </List>
         </div>
-        {types && types.length === 1 && (
+        {types && types.length === 1 && createButton && (
           <div className={styles.footer}>
             <IntentButton bleed color="primary" kind="simple" intent="create" params={{type: types[0]}}>
               {createButtonText || `Create new ${types[0]}`}
